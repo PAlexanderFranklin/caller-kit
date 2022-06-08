790c1e86ed82a2913e5786b511c8285584c3b3ff
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-06-08 14:50:45 -0700

    Add target to config

--- src/App.svelte
+++ src/App.svelte
@@ -3,21 +3,44 @@
 
 	import { Router, Route, createHistory } from "svelte-navigator";
   import createHashSource from "./lib/utils/hashHistory.js";
-  import SkynetContextProvider from "./lib/utils/SkynetContextProvider.svelte";
-  import Login from "./lib/Components/Login.svelte";
+  // import SkynetContextProvider from "./lib/utils/SkynetContextProvider.svelte";
+  // import Login from "./lib/Components/Login.svelte";
+
+  import * as kernel from "libkernel";
+
+  import {
+    IdentityDAC,
+    ProfileDAC,
+    SocialDAC,
+    FeedDAC,
+  } from "skynet-dacs-library";
   
   const hash = createHistory(createHashSource());
+
+  // const identityDAC = new IdentityDAC();
+
+  // const profileDAC = new ProfileDAC();
+  // const socialDAC = new SocialDAC();
+  // const feedDAC = new FeedDAC();
+
+  let loading = true
+  kernel.init().then(loading = false);
+
 </script>
 
-<Router history={hash}>
-  <SkynetContextProvider>
-    <Route path="login" component={Login} />
+{#if loading}
+  <div>The application is loading.</div>
+{:else}
+  <Router history={hash}>
+    <!-- <SkynetContextProvider> -->
+      <!-- <Route path="login" component={Login} /> -->
 
-    <Route>
-      <p>This is the default route.</p>
-    </Route>
-  </SkynetContextProvider>
-</Router>
+      <Route>
+        <p>This is the default route.</p>
+      </Route>
+    <!-- </SkynetContextProvider> -->
+  </Router>
+{/if}
 
 <style>
   :root {
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2020"
+  },
   plugins: [svelte()],
 	optimizeDeps: { exclude: ["svelte-navigator"] },
 })
