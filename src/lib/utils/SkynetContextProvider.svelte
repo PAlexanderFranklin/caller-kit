39b5ce8fd7d13ee18ba43ec65d6fb295ce28d6f5
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-06-14 19:02:53 -0700

    Add skynetcontext

--- src/lib/utils/SkynetContextProvider.svelte
+++ src/lib/utils/SkynetContextProvider.svelte
@@ -1,4 +1,33 @@
 <script>
+import { writable, setContext } from "svelte/store";
+import { init as kernelInit, openAuthWindow } from 'libkernel';
+
+let userAuthStatus = writable(false)
+let authInProgress = writable(false)
+
+// on first load, check authentication status of user
+// this also calls init()
+$: checkAuthStatus();
+
+const checkAuthStatus = async () => {
+  $authInProgress = true;
+  const result = await kernelInit();
+  result ? $userAuthStatus = false : $userAuthStatus = true;
+  $authInProgress = false;
+};
+
+const login = async () => {
+  $authInProgress = true;
+  await openAuthWindow();
+  checkAuthStatus();
+  $authInProgress = false;
+};
+
+setContext("skynetContext", {
+  userAuthStatus,
+  login,
+  authInProgress,
+})
 </script>
 
 
