4d32f8d21db28b13320b426941b928facbcae479
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-06-22 23:32:34 -0700

    Fix adding calls to dance

--- src/lib/Components/DanceCreator/DanceCreator.svelte
+++ src/lib/Components/DanceCreator/DanceCreator.svelte
@@ -8,18 +8,18 @@ import DanceGraph from "./DanceGraph/DanceGraph.svelte";
 
 const newDance = writable({
   dance: {instructions: [[{beats: 2}, {beats: 4, delay: 4}, {beats: 20}], [{beats: 3}, {beats: 4}]]},
-  selection: {group: 0, callIndex: 0, delay: false},
+  selection: {group: 0, call: 0, delay: false},
   duration: 0,
   indexing: {group: 0},
 });
 setContext("newDance", {newDance})
 
 function addCall(call) {
-  console.log(call);
   $newDance.dance.instructions[$newDance.selection.group].splice(
-    $newDance.selection.callIndex, 0, call
+    $newDance.selection.call, 0, call
   );
   $newDance.dance.instructions = [...$newDance.dance.instructions];
+  $newDance.selection = {...$newDance.selection, call: $newDance.selection.call + 1, delay: false};
 }
 
 $: $newDance.indexing = {group: 0};
